ff9e95d5b5908b810fb25b90b474aad692024a62
Santiago PMontero <60758654+santiagopm84@users.noreply.github.com>
2022-01-31 20:53:11 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function loadComponents() {
+  $(".content_head").load("components/head.html");
   $(".wrapper_header").load("components/header.html");
   $(".wrapper_footer").load("components/footer.html");
   $(".wrapper_experience").load("components/experience.html");
   $(".wrapper_interest").load("components/interest.html");
 }
+
